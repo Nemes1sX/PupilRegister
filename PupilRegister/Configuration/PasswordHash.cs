4e1a02504e3d4cb35aef3f3159867997e2b01d65
Nemes1sX <kazstonk@gmail.com>
2022-03-10 11:31:28 +0200

    Remove unneccessary password check in veryfing password hash.

--- PupilRegister/Configuration/PasswordHash.cs
+++ PupilRegister/Configuration/PasswordHash.cs
@@ -20,7 +20,6 @@ namespace PupilRegister.Configuration
 
         public static bool VerifyPasswordHash(string password, byte[] storedHash, byte[] storedSalt)
         {
-            if (password == null) throw new ArgumentNullException("password");
             if (string.IsNullOrWhiteSpace(password)) throw new ArgumentException("Value cannot be empty or whitespace only string.", "password");
             if (storedHash.Length != 64) throw new ArgumentException("Invalid length of password hash (64 bytes expected).", "passwordHash");
             if (storedSalt.Length != 128) throw new ArgumentException("Invalid length of password salt (128 bytes expected).", "passwordHash");
